a593311d981d648b76ffe701168699d115034782
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-20 15:56:19 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -72,9 +72,13 @@ class FileStorage:
 
     def count(self, cls=None):
         """Count number of cls object"""
-        if cls in classes:
-            return len(self.all(cls))
-        return len(self.all())
+        count = 0
+        if cls is not None:
+            for val in self.all(cls):
+                if cls == val.__class__.__name__ or cls == val.__class__:
+                    count += 1
+            return count
+        return self.all().__len__()
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
