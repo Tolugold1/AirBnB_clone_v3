f49b6162688e4f529363c1a1540f8fa5d0293b52
Sodiq179 <babawalesodiq996@gmail.com>
2022-10-22 19:28:17 +0100

    [Feature]:Endpoint to retrieve number of each objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,27 @@
 
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status')
 def status():
     """return a status code in JSON format"""
     return jsonify({"status": "OK"})
+
+@paa_views.route("/stats")
+def stats():
+    """
+    Return the count of each 
+    object in json format
+    """
+
+    objCount = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+
+    return jsonify(objCount)
