ab7003068519881861033cf0d3a2a38ae6521a29
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-22 15:54:07 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,14 +10,15 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({"error": "Not found"}), 404)
-
 @app.teardown_appcontext
 def remove_session(response_or_exc):
     """Remove the surrent session"""
     storage.close()
+    
+@app.errorhandler(404)
+def not_found(error):
+    """404 page"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == '__main__':
     HBNB_API_HOST = getenv('HBNB_API_HOST')
