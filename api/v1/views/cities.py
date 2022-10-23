850be1634c51d790ae141c533efde8a41b0000e7
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-23 19:57:15 +0100

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,7 +6,8 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 
-@app_views.route('/states/<state_id>/cities', strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', 
+                 strict_slashes=False)
 def city(state_id):
     """Retrieves the list of all City objects of a State"""
     allState_city = []
@@ -17,7 +18,8 @@ def city(state_id):
             allState_city.append(city.to_dict())
     return jsonify(allState_city)
 
-@app_views.route('/cities/<city_id>', strict_slashes=False)
+@app_views.route('/cities/<city_id>', 
+                 strict_slashes=False)
 def city_obj(city_id):
     """Retrieves a City object"""
     city = storage.get(City, city_id)
@@ -26,7 +28,8 @@ def city_obj(city_id):
     else:
         return city.to_dict()
     
-@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/cities/<city_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_city(city_id):
     """DElete a city object"""
     empty_dict = {}
@@ -37,7 +40,8 @@ def delete_city(city_id):
     storage.save()
     return empty_dict
         
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'], 
+                 strict_slashes=False)
 def post_new_city(state_id):
     """create new city under the specified state_id"""
     city_body_req = request.get_json()
@@ -53,7 +57,8 @@ def post_new_city(state_id):
     storage.save()
     return newCity.to_dict(), 201
 
-@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/cities/<city_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_city(city_id):
     """update existing city in the db or storage"""
     updates = request.get_json()
