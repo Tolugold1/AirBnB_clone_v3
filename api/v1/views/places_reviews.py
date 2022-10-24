1e5525565908a2d25ec02e46676d47d6608ddc64
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-24 20:13:12 +0100

    Update places_reviews.py

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""State view"""
-from flask import Flask, jsonify, abort, request, make_response
+"""Review view"""
+from flask import Flask, jsonify, abort, request
 from AirBnB_clone_v3.models import review
 from api.v1.views import app_views
 from models import storage
