2afe6062d0cda49936e0b9cad301692d27a70500
Tolulope Adeleke <toluadelekesteve@gmail.com>
2022-10-24 17:49:34 +0100

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,8 +8,5 @@ from api.v1.views.index import *
 from api.v1.views.cities import *
 from api.v1.views.states import *
 from api.v1.views.users import *
-<<<<<<< HEAD
-=======
 from api.v1.views.amenities import *
->>>>>>> 05a98f274eb2d0102aab8706fea7f5ac9df2868e
 from api.v1.views.places import *
