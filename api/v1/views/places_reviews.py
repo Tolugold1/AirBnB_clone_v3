b9a798d925c72b088755e13a990aec0ade5d9eaf
Sodiq <65002222+Sodiq179@users.noreply.github.com>
2022-10-25 21:00:58 +0100

    [Update] import necessary libraries

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Review view"""
-from flask import Flask, jsonify, abort, request. make_response
+from flask import Flask, jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage
 from models.place import Place
